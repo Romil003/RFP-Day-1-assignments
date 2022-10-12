76016908710e569d3dbdc3dd3a8ba0cabd98fb00
Romil Ramesh Ghadge <111551511+Romil003@users.noreply.github.com>
2022-10-12 20:14:22 +0530

    Display any message

--- src/DisplayAnyMessages.java
+++ src/DisplayAnyMessages.java
@@ -1,6 +1,6 @@
 public class DisplayAnyMessages {
     public static void main(String[] args) {
 
-        System.out.println("Hi !!! Romil Here...");
+        System.out.println("Hi  !!! Romil Here...");
     }
 }
