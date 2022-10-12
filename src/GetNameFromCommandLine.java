4c07d92914fc7f39da6491a6901a1d8f5f46f91a
Romil Ramesh Ghadge <111551511+Romil003@users.noreply.github.com>
2022-10-12 20:15:45 +0530

    Get name from command-line

--- src/GetNameFromCommandLine.java
+++ src/GetNameFromCommandLine.java
@@ -5,6 +5,7 @@ public class GetNameFromCommandLine {
             String b = args[1];
             String c = args[2];
 
+                
             System.out.println(a);
             System.out.println(b);
             System.out.println(c);
