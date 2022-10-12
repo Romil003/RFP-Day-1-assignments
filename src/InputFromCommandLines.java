02863cb4518afe4d68c330ec849c0dade37d828a
Romil Ramesh Ghadge <111551511+Romil003@users.noreply.github.com>
2022-10-12 20:16:21 +0530

    Get integer from command-line

--- src/InputFromCommandLines.java
+++ src/InputFromCommandLines.java
@@ -5,6 +5,7 @@ public class InputFromCommandLines {
         int l = Integer.parseInt(args[1]);
         int m = Integer.parseInt(args[2]);
 
+        
         System.out.println(k);
         System.out.println(l);
         System.out.println(m);
