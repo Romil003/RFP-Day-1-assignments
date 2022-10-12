e6ba472517410e749dce47fee4ad945ebbf811dd
Romil Ramesh Ghadge <111551511+Romil003@users.noreply.github.com>
2022-10-12 20:13:47 +0530

    Printing default values

--- src/DefaultValues.java
+++ src/DefaultValues.java
@@ -10,6 +10,7 @@ public class DefaultValues {
 
     public static void main(String[] args) {
 
+        
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
